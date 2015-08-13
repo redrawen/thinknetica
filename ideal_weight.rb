c891ab63d5bb949f2ee835d171e448f161dc60b9
Red_rawen <prokopenko.viktory@gmail.com>
2015-08-13 23:34:58 +0300

    first program commit

---
+++ ideal_weight.rb
@@ -0,0 +1,9 @@
+puts "Введите ваше имя"
+name = gets.chomp
+puts "Введите ваш рост"
+height = gets.chomp
+ideal_weight = height.to_i - 110
+if ideal_weight < 0 
+puts "#{name}, Ваш идеальный вес уже оптимальный!"
+else puts"#{name}, Ваш идеальный вес неоптимальный!"
+end
